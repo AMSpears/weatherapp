12b8503d7c6263b225bc95a2991e6d071015d3ab
Angie Spears <angie.spears809@gmail.com>
2018-08-04 21:01:21 -0400

    add initial style to other cities

--- src/components/CityThree/CityThree.js
+++ src/components/CityThree/CityThree.js
@@ -3,6 +3,8 @@ import Header from "../Header/Header"
 
 
 const API_KEY = "d0c74e4757653dd7d26fe20813b354d8"
+const dateFormat = require('dateformat');
+const now = new Date();
 
 class CityThree extends Component {
     state = {
@@ -27,121 +29,149 @@ class CityThree extends Component {
             weatherResults
         } = this.state;
         return ( 
-            <div> 
-            <Header/>
-            {
-              this.state.weatherResults.length !== 0 &&
-                <div className = "container" key = { weatherResults.city.id } >
-                    <h2> { weatherResults.city.name }</h2> 
-                    <h3>Today</h3>
-                        <table>
-                            <tr>
-                                <th>Date/Time</th>
-                                <th>Description</th>
-                                <th> Temperature</th>
-                                <th>Pressure</th>
-                                <th> Humidity</th>
-                                <th> Wind </th>
-                            </tr>
-                            <tr>
-                                <td>{weatherResults.list[0].dt_txt}</td>
-                                <td>{weatherResults.list[0].weather[0].description}</td>
-                                <td>{weatherResults.list[0].main.temp} </td>
-                                <td>{weatherResults.list[0].main.pressure}</td>
-                                <td>{weatherResults.list[0].main.humidity}</td>
-                                <td> {weatherResults.list[0].wind.speed}</td>
-                            </tr> 
-                            <tr>
-                                <td>{weatherResults.list[1].dt_txt}</td>
-                                <td>{weatherResults.list[1].weather[0].description}</td>
-                                <td>{weatherResults.list[1].main.temp} </td>
-                                <td>{weatherResults.list[1].main.pressure}</td>
-                                <td>{weatherResults.list[1].main.humidity}</td>
-                                <td> {weatherResults.list[1].wind.speed}</td>
-                            </tr>
-                            <tr>
-                                <td>{weatherResults.list[2].dt_txt}</td>
-                                <td>{weatherResults.list[2].weather[0].description}</td>
-                                <td>{weatherResults.list[2].main.temp} </td>
-                                <td>{weatherResults.list[2].main.pressure}</td>
-                                <td>{weatherResults.list[2].main.humidity}</td>
-                                <td> {weatherResults.list[2].wind.speed}</td>
-                            </tr>
-                            <tr>
-                                <td>{weatherResults.list[3].dt_txt}</td>
-                                <td>{weatherResults.list[3].weather[0].description}</td>
-                                <td>{weatherResults.list[3].main.temp} </td>
-                                <td>{weatherResults.list[3].main.pressure}</td>
-                                <td>{weatherResults.list[3].main.humidity}</td>
-                                <td> {weatherResults.list[3].wind.speed}</td>
-                            </tr>
-                            <tr>
-                                <td>{weatherResults.list[4].dt_txt}</td>
-                                <td>{weatherResults.list[4].weather[0].description}</td>
-                                <td>{weatherResults.list[4].main.temp} </td>
-                                <td>{weatherResults.list[4].main.pressure}</td>
-                                <td>{weatherResults.list[4].main.humidity}</td>
-                                <td> {weatherResults.list[4].wind.speed}</td>
-                             </tr>
-                             <tr>
-                                <td>{weatherResults.list[5].dt_txt}</td>
-                                <td>{weatherResults.list[5].weather[0].description}</td>
-                                <td>{weatherResults.list[5].main.temp} </td>
-                                <td>{weatherResults.list[5].main.pressure}</td>
-                                <td>{weatherResults.list[5].main.humidity}</td>
-                                <td> {weatherResults.list[5].wind.speed}</td>
-                            </tr>
-                        </table>
-                        
-                        <h3>Week overview</h3>
-                            <div className ="week_container">
-                                <div className= "days_container">
-                                  <h4> Date/Time <br/> {weatherResults.list[6].dt_txt} </h4>
-                                   <tr>
-                                        <p>{weatherResults.list[6].weather[0].description}</p>
-                                        <p>{weatherResults.list[6].main.temp} </p>
-                                        <p>{weatherResults.list[6].main.pressure}</p>
-                                        <p>{weatherResults.list[6].main.humidity}</p>
-                                        <p> {weatherResults.list[6].wind.speed}</p>
-                                  </tr>
-                                 </div>
-                                <div className= "days_container">
-                                  <h4> Date/Time <br/> {weatherResults.list[14].dt_txt} </h4>
-                                    <tr>
-                                        <p>{weatherResults.list[14].weather[0].description}</p>
-                                        <p>{weatherResults.list[14].main.temp} </p>
-                                        <p>{weatherResults.list[14].main.pressure}</p>
-                                        <p>{weatherResults.list[14].main.humidity}</p>
-                                        <p> {weatherResults.list[14].wind.speed}</p>
-                                    </tr>
+            <div>
+            <nav>
+                <Header/>
+            </nav>
+            { this.state.weatherResults.length !== 0 &&
+            <div className = "container" key = { weatherResults.city.id } >
+                <h1>{ weatherResults.city.name } /  {Math.round(weatherResults.list[0].main.temp)}&#176;</h1> 
+                    <div className= "cities_weather_container">
+                        <div className= "cities_weather_summary">
+                             <h3>Today / {dateFormat(now, "h:ss TT")}</h3>
+                                <p id= "description">{weatherResults.list[0].weather[0].description}</p>
+                                <p id= "temp"> {Math.round(weatherResults.list[0].main.temp)}&#176;</p>
+                                <div className = "weather_details">
+                                <p>Min: {Math.round(weatherResults.list[0].main.temp_min)}&#176;</p>
+                                <p >Max: {Math.round(weatherResults.list[0].main.temp_max)}&#176;</p>
+                                <p>Humidity: {weatherResults.list[0].main.humidity}%</p>
+                                <p> SW {Math.round(weatherResults.list[0].wind.speed)} mph</p>
+                            </div>
+                        </div>
+                         <div className = "cities_weather_summary">
+                            <table>
+                                <h3>Hourly</h3>
+                                <tr>
+                                    <th>Time</th>
+                                    <th>Description</th>
+                                    <th> Temperature</th>
+                                    <th> Min/Max</th>
+                                    <th> Humidity</th>
+                                    <th> Wind </th>
+                                </tr>
+                                <tr>
+                                    <td> {dateFormat(weatherResults.list[1].dt_txt, "h TT")} </td>
+                                    <td>{weatherResults.list[1].weather[0].description}</td>
+                                    <td>{Math.round(weatherResults.list[1].main.temp)}&#176;</td>
+                                    <td>{Math.round(weatherResults.list[1].main.temp_min)}&#176; / 
+                                        {Math.round(weatherResults.list[1].main.temp_max)}&#176;</td>
+                                    <td>{weatherResults.list[1].main.humidity}%</td>
+                                    <td> SW {Math.round(weatherResults.list[1].wind.speed)} mph</td>
+                                </tr>
+                                <tr>
+                                    <td>{dateFormat(weatherResults.list[2].dt_txt, "h TT")}</td>
+                                    <td>{weatherResults.list[2].weather[0].description}</td>
+                                    <td>{Math.round(weatherResults.list[2].main.temp)}&#176;</td>
+                                    <td>{Math.round(weatherResults.list[2].main.temp_min)}&#176; / 
+                                        {Math.round(weatherResults.list[2].main.temp_max)}&#176;</td>
+                                    <td>{weatherResults.list[2].main.humidity}%</td>
+                                    <td> SW {Math.round(weatherResults.list[2].wind.speed)} mph</td>
+                                </tr>
+                                <tr>
+                                    <td>{dateFormat(weatherResults.list[3].dt_txt,"h TT")}</td>
+                                    <td>{weatherResults.list[3].weather[0].description}</td>
+                                    <td>{Math.round(weatherResults.list[3].main.temp)}&#176;</td>
+                                    <td>{Math.round(weatherResults.list[3].main.temp_min)}&#176; / 
+                                        {Math.round(weatherResults.list[3].main.temp_max)}&#176;</td> 
+                                    <td>{weatherResults.list[3].main.humidity}%</td>
+                                    <td> SW {Math.round(weatherResults.list[3].wind.speed)} mph</td>
+                                </tr>
+                                <tr>
+                                    <td>{dateFormat(weatherResults.list[4].dt_txt,"h TT")}</td>
+                                    <td>{weatherResults.list[4].weather[0].description}</td>
+                                    <td>{Math.round(weatherResults.list[4].main.temp)}&#176;</td>
+                                    <td>{Math.round(weatherResults.list[4].main.temp_min)}&#176; / 
+                                        {Math.round(weatherResults.list[4].main.temp_max)}&#176;</td>
+                                    <td>{weatherResults.list[4].main.humidity}%</td>
+                                    <td> SW {Math.round(weatherResults.list[4].wind.speed)} mph</td>
+                                </tr>
+                                <tr>
+                                    <td>{dateFormat(weatherResults.list[5].dt_txt,"h TT")}</td>
+                                    <td>{weatherResults.list[5].weather[0].description}</td>
+                                    <td>{Math.round(weatherResults.list[5].main.temp)}&#176;</td>
+                                    <td>{Math.round(weatherResults.list[5].main.temp_min)}&#176;/             
+                                        {Math.round(weatherResults.list[5].main.temp_max)}&#176;</td>
+                                    <td>{weatherResults.list[5].main.humidity}%</td>
+                                    <td> SW {Math.round(weatherResults.list[5].wind.speed)} mph</td>
+                                </tr>
+                            </table>
+                         </div>
+                    </div>     
+
+                    <h3>Week Overview</h3>
+                    <div className ="week_container">
+                        <div className= "days_container">
+                            <h4 id = "weekday_header">Tomorrow,<br/>
+                                {dateFormat(weatherResults.list[6].dt_txt,"dddd, mmmm dS")} 
+                            </h4>
+                            <div className= "weather_details">
+                                <p id = "temp" >{Math.round(weatherResults.list[6].main.temp)}&#176;</p>
+                                <p id = "description">{weatherResults.list[6].weather[0].description}</p>
+                                <p>Min: {Math.round(weatherResults.list[6].main.temp_min)}&#176;</p> 
+                                <p>Max: {Math.round(weatherResults.list[6].main.temp_max)}&#176;</p>
+                                <p>Humidity: {weatherResults.list[6].main.humidity}%</p>
+                                <p> SW {Math.round(weatherResults.list[6].wind.speed)} mph</p>
+                            </div>
+                            </div>
+                            <div className= "days_container">
+                                <h4 id= "weekday_header">
+                                    {dateFormat(weatherResults.list[14].dt_txt,"dddd")},<br/>
+                                    {dateFormat(weatherResults.list[14].dt_txt,"mmmm dS")}
+                                 </h4>
+                                 <p id= "temp">{Math.round(weatherResults.list[14].main.temp)}&#176;</p>
+                                <div className= "weather_details">
+                                    <p id= "description"> {weatherResults.list[14].weather[0].description}</p>
+                                    <p>Min: {Math.round(weatherResults.list[14].main.temp_min)}&#176; </p>
+                                    <p>Max: {Math.round(weatherResults.list[14].main.temp_max)}&#176;</p>
+                                    <p>Humidity: {weatherResults.list[14].main.humidity}%</p>
+                                    <p> SW {Math.round(weatherResults.list[14].wind.speed)} mph</p>
                                 </div>
-                                <div className= "days_container">
-                                   <h4> Date/Time <br/> {weatherResults.list[22].dt_txt} </h4>
-                                    <tr>
-                                        <p>{weatherResults.list[22].weather[0].description}</p>
-                                        <p>{weatherResults.list[22].main.temp} </p>
-                                        <p>{weatherResults.list[22].main.pressure}</p>
-                                        <p>{weatherResults.list[22].main.humidity}</p>
-                                        <p> {weatherResults.list[22].wind.speed}</p>
-                                    </tr>
+                            </div>
+                            <div className= "days_container">
+                                    <h4 id= "weekday_header">
+                                    {dateFormat(weatherResults.list[22].dt_txt,"dddd")},<br/>
+                                    {dateFormat(weatherResults.list[22].dt_txt,"mmmm dS")}
+                                    </h4>
+                                    <p id = "temp">{Math.round(weatherResults.list[22].main.temp)}&#176;</p>
+                                <div className= "weather_details">
+                                    <p id= "description">{weatherResults.list[22].weather[0].description}</p>
+                                    <p>Min: {Math.round(weatherResults.list[22].main.temp_min)}&#176;</p> 
+                                    <p>Max: {Math.round(weatherResults.list[22].main.temp_max)}&#176;</p>
+                                    <p>Humidity: {weatherResults.list[22].main.humidity}%</p>
+                                    <p> SW {Math.round(weatherResults.list[22].wind.speed)} mph</p>
                                 </div>
-                                <div className= "days_container">
-                                    <h4> Date/Time <br/> {weatherResults.list[30].dt_txt} </h4>
-                                     <tr>
-                                        <p>{weatherResults.list[30].weather[0].description}</p>
-                                        <p>{weatherResults.list[30].main.temp} </p>
-                                        <p>{weatherResults.list[30].main.pressure}</p>
-                                        <p>{weatherResults.list[30].main.humidity}</p>
-                                        <p> {weatherResults.list[30].wind.speed}</p>
-                                    </tr>
-                                </div> 
-                     </div>
-                </div> 
-              }
-         </div>
-        );
+                            </div>
+                            <div className= "days_container">
+                                    <h4 id= "weekday_header">
+                                    {dateFormat(weatherResults.list[30].dt_txt,"dddd")},<br/>
+                                    {dateFormat(weatherResults.list[30].dt_txt,"mmmm dS")}
+                                    </h4>                                    
+                                    <p id= "temp" >{Math.round(weatherResults.list[30].main.temp)}&#176;</p>
+                                 <div className= "weather_details">
+                                    <p id= "description">{weatherResults.list[30].weather[0].description}</p>
+                                    <p>Min: {Math.round(weatherResults.list[30].main.temp_min)}&#176;</p> 
+                                    <p>Max: {Math.round(weatherResults.list[30].main.temp_max)}&#176;</p>
+                                    <p>Humidity: {weatherResults.list[30].main.humidity}%</p>
+                                    <p> SW {Math.round(weatherResults.list[30].wind.speed)} mph</p>
+                                </div>
+                             </div> 
+                         </div>
+                     </div> 
+                    }
+                </div>
+            );
+        }
     }
-}
 
 
 export default CityThree
